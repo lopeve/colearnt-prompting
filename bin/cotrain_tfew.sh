a079a6fb42c828e40c018d065c2afe890dea197e
lopeve <135252518+lopeve@users.noreply.github.com>
2021-09-04 14:32:39 +0000

    remove leftover

--- bin/cotrain_tfew.sh
+++ bin/cotrain_tfew.sh
@@ -4,4 +4,4 @@ for seed in 0 1 32 42 1024
 do
     for dataset in rte cb boolq
     do
-        python -m sr
+        python -m src.cotrain -c t03b.json+${dataset}.json+ia3.json -k exp_name=cotrain_ia3_${dataset}_seed${seed} seed=${seed} few_shot=False allow_skip_exp=True 
