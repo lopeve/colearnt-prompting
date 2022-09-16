37fb1e663e666c89d154eabc7f0cf92fdbdfb604
lopeve <135252518+lopeve@users.noreply.github.com>
2022-09-16 16:31:51 +0000

    Added a newline

--- src/cotrain_gpt.py
+++ src/cotrain_gpt.py
@@ -146,3 +146,79 @@ def main(config):
         model = LabelModel(
             config,
             num_feat=num_feat,
+            cbu_params=cbu_params,
+            dataset_reader=dataset_reader,
+        )
+
+        dm = LabelModelDataModule(config, ds_dict)
+
+        ckpt_dir=os.path.join(config.exp_dir, 'labelmodel')
+        checkpoint_callback = ModelCheckpoint(dirpath=ckpt_dir,
+                                              save_top_k=1,
+                                              monitor="val_balanced_acc",
+                                              mode='max')
+
+        # fine-tune label model
+        trainer = Trainer(
+            max_epochs=config.num_epochs,
+            accelerator='gpu',
+            devices=1,
+            check_val_every_n_epoch=config.eval_epoch_interval,
+            callbacks=[checkpoint_callback],
+        )
+
+        trainer.fit(model, dm)
+
+        print(f"got best model path {trainer.checkpoint_callback.best_model_path}")
+
+        if config.cotrain_load_best:
+            load_path = trainer.checkpoint_callback.best_model_path
+            del model
+            del trainer
+            del dm
+            gc.collect()
+
+            torch.cuda.empty_cache()
+            model = LabelModel.load_from_checkpoint(load_path)
+
+    # get the ds dict for the label model one last time so that
+    # we write out the metrics for the last run.
+    ds_dict = get_dsdict_labelmodel(
+        config, dataset_reader,
+        model.to('cuda')
+    )
+    # loop is done.
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config_files", required=True)
+    parser.add_argument("-k", "--kwargs", nargs="*", action=ParseKwargs, default={})
+    args = parser.parse_args()
+
+    datasets.disable_caching()
+
+    # configure logging at the root level of Lightning
+    #logging.getLogger("pytorch_lightning").setLevel(logging.DEBUG)
+
+    config = Config(args.config_files, args.kwargs)
+    print(f"Start experiment {config.exp_name}")
+    # Setup config
+    assert config.compute_strategy in ["none", "ddp", "deepspeed_stage_3_offload", "deepspeed_stage_3"]
+    if config.fishmask_mode == "create":
+        print("Detecting fishmask_mode=create, override batch_size, num_step, fishmask_path")
+        config.batch_size = 1
+        config.num_steps = config.num_shot
+        config.eval_before_training = False
+        config.fishmask_path = None
+
+    print(config.to_json())
+
+    if config.allow_skip_exp and os.path.exists(config.finish_flag_file):
+        print(f"Skip finished experiment {config.exp_name}")
+    else:
+        print(f"Mark experiment {config.exp_name} as claimed")
+        with open(config.finish_flag_file, "a+") as f:
+            f.write(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + "\n")
+        set_seeds(config.seed)
+        main(config)
---
+++ src/data/__init__.py
@@ -0,0 +1,3 @@
+from .data_module import (
+    FinetuneDataModule,
+    PretrainDataModule
