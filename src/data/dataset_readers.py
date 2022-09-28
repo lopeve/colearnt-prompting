e7bb9b6771c6ed31da626b3caca494bf78de0776
lopeve <135252518+lopeve@users.noreply.github.com>
2022-09-28 20:27:41 +0000

    #66

--- src/data/dataset_readers.py
+++ src/data/dataset_readers.py
@@ -6,4 +6,10 @@ from promptsource.templates import DatasetTemplates
 import pkg_resources
 from promptsource import templates
 import csv
-from typing import D
+from typing import Dict, List, Optional, Tuple
+import re
+import pandas as pd
+from sklearn.metrics import balanced_accuracy_score
+
+def get_dataset_reader(config):
+    dataset_class = 
