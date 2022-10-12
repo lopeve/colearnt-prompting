ce0d8933a81a668c8d9690e7120e4e63810c60c8
lopeve <135252518+lopeve@users.noreply.github.com>
2022-10-12 10:16:11 +0000

    Added the files

--- src/data/dataset_readers.py
+++ src/data/dataset_readers.py
@@ -88,4 +88,7 @@ TASK_BLACKLIST = [
     # 'quail_context_description_question_answer_text',
     # 'quail_context_description_question_text',
     # 'quail_context_question_answer_description_text',
-    # 'quail_context_question_description_answe
+    # 'quail_context_question_description_answer_id',
+    # 'quail_context_question_description_text',
+    # 'quail_description_context_question_answer_id',
+    # 'quail_description_context_question_answer_text',
