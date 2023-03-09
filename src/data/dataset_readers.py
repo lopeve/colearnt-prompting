61c81fca8216491c54b446dbf63f61ca0f66ed0c
lopeve <135252518+lopeve@users.noreply.github.com>
2023-03-09 05:39:01 +0000

    JavaDoc update

--- src/data/dataset_readers.py
+++ src/data/dataset_readers.py
@@ -637,4 +637,6 @@ class RaftReader(object):
         for idx, prediction in zip(idxs, predictions):
             data.append({"ID": idx, "Label": self.answer_choices[prediction]})
         result_df = pd.DataFrame(data=data, columns=["ID", "Label"]).astype({"ID": int, "Label": str})
-        result_df.t
+        result_df.to_csv(self.config.dev_pred_file, index=False)
+        matching = [a == b for a, b in zip(accumulated["prediction"], accumulated["label"])]
+        accuracy = sum(matc
