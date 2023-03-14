8da5938eb5ef6463b24ef42a7661314910b888fe
lopeve <135252518+lopeve@users.noreply.github.com>
2023-03-14 07:50:07 +0000

    rake

--- src/models/BERT.py
+++ src/models/BERT.py
@@ -4,4 +4,9 @@ import torch
 import torch.nn.functional as F
 import torch.distributed as dist
 from pytorch_lightning import LightningModule
-from typing i
+from typing import Optional
+from torch.optim import AdamW
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    get_lin
