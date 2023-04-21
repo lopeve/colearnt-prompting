303415b3f42b9f11e9707f2e86f4778b46cb6fcd
lopeve <135252518+lopeve@users.noreply.github.com>
2023-04-21 05:46:01 +0000

    Updated CSS.

--- src/models/adapters.py
+++ src/models/adapters.py
@@ -39,3 +39,164 @@ class T5LayerSelfAttentionWithAdapter(nn.Module):
         self.adapter = get_adapter(config.adapter_type)(config, transformer_config)
         self.layer_norm = T5LayerSelfAttention.layer_norm
         self.dropout = T5LayerSelfAttention.dropout
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        position_bias=None,
+        layer_head_mask=None,
+        past_key_value=None,
+        use_cache=False,
+        output_attentions=False,
+    ):
+        normed_hidden_states = self.layer_norm(hidden_states)
+        attention_output = self.SelfAttention(
+            normed_hidden_states,
+            mask=attention_mask,
+            position_bias=position_bias,
+            layer_head_mask=layer_head_mask,
+            past_key_value=past_key_value,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+        )
+        adapter_output = self.adapter(attention_output[0])
+        hidden_states = hidden_states + self.dropout(adapter_output)
+        outputs = (hidden_states,) + attention_output[1:]  # add attentions if we output them
+        return outputs
+
+
+class T5LayerCrossAttentionWithAdapter(nn.Module):
+    def __init__(self, T5LayerCrossAttention, config, transformer_config):
+        super().__init__()
+        self.EncDecAttention = T5LayerCrossAttention.EncDecAttention
+        self.adapter = get_adapter(config.adapter_type)(config, transformer_config)
+        self.layer_norm = T5LayerCrossAttention.layer_norm
+        self.dropout = T5LayerCrossAttention.dropout
+
+    def forward(
+        self,
+        hidden_states,
+        key_value_states,
+        attention_mask=None,
+        position_bias=None,
+        layer_head_mask=None,
+        past_key_value=None,
+        use_cache=False,
+        query_length=None,
+        output_attentions=False,
+    ):
+        normed_hidden_states = self.layer_norm(hidden_states)
+        attention_output = self.EncDecAttention(
+            normed_hidden_states,
+            mask=attention_mask,
+            key_value_states=key_value_states,
+            position_bias=position_bias,
+            layer_head_mask=layer_head_mask,
+            past_key_value=past_key_value,
+            use_cache=use_cache,
+            query_length=query_length,
+            output_attentions=output_attentions,
+        )
+        adapter_output = self.adapter(attention_output[0])
+        layer_output = hidden_states + self.dropout(adapter_output)
+        outputs = (layer_output,) + attention_output[1:]  # add attentions if we output them
+        return outputs
+
+
+def modify_with_adapters(transformer, config):
+    for m_name, module in dict(transformer.named_modules()).items():
+        if re.fullmatch(".*block[.][0-9]*", m_name):
+            layer = nn.ModuleList()
+            if (config.compacter_add_compacter_in_self_attention):
+                layer.append(
+                    T5LayerSelfAttentionWithAdapter(
+                        module.layer[0],
+                        config,
+                        transformer.config,
+                    )
+                )
+            else:
+                layer.append(module.layer[0])
+            if module.is_decoder:
+                if (config.compacter_add_compacter_in_cross_attention):
+                    layer.append(
+                        T5LayerCrossAttentionWithAdapter(
+                            module.layer[1],
+                            config,
+                            transformer.config,
+                        )
+                    )
+                else:
+                    layer.append(module.layer[1])
+
+            layer.append(
+                T5LayerFFWithAdapter(
+                    module.layer[2] if module.is_decoder else module.layer[1],
+                    config,
+                    transformer.config,
+                )
+            )
+            module.layer = layer
+    return transformer
+
+
+if __name__ == "__main__":
+    from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--adapter_type",
+        required=True,
+        type=str,
+        choices=["normal", "lowrank", "compacter"],
+    )
+    args = parser.parse_args()
+
+    class AdapterConfig:
+        def __init__(self, adapter_type):
+            self.adapter_type = adapter_type
+
+            if self.adapter_type == "normal":
+                # Adapter Config
+                self.adapter_reduction_factor = 16
+                self.adapter_non_linearity = "relu"
+                self.normal_adapter_residual = True
+                self.add_compacter_in_attention = True
+            elif self.adapter_type == "compacter":
+                # Compacter
+                self.adapter_reduction_factor = 16
+                self.adapter_non_linearity = "relu"
+                self.compacter_hypercomplex_division = 4
+                self.compacter_learn_phm = True
+                self.compacter_hypercomplex_nonlinearity = "xyz"
+                self.compacter_shared_phm_rule = False
+                self.compacter_factorized_phm = True
+                self.compacter_shared_W_phm = False
+                self.compacter_factorized_phm_rule = False
+                self.compacter_phm_c_init = "xyz"
+                self.compacter_phm_rank = 1
+                self.compacter_phm_init_range = 0.0001
+                self.compacter_kronecker_prod = False
+                self.compacter_adapter_non_linearity = "gelu_new"
+                self.compacter_add_compacter_in_attention = True
+
+            self.trainable_param_names = ".*layer_norm.*|.*adapter.*"
+
+    config = AdapterConfig(args.adapter_type)
+    model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
+    tokenizer = AutoTokenizer.from_pretrained("t5-small")
+
+    input_seq = tokenizer(
+        ["Applies a linear transformation to the incoming data."],
+        return_tensors="pt",
+    )
+    target_seq = tokenizer(
+        ["Parameters: in_features - size of each input sample. out_features - size of each output sample."],
+        return_tensors="pt",
+    )
+
+    print("Old model")
+    # print(model)
