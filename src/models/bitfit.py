cd5899f08a82f97ffd0a481ff3ff01e6b42f9162
lopeve <135252518+lopeve@users.noreply.github.com>
2023-04-23 06:52:26 +0000

    Merging nightly changes from rice-2.5 into trunk

--- src/models/bitfit.py
+++ src/models/bitfit.py
@@ -4,4 +4,6 @@ import re
 
 
 def modify_with_bitfit(transformer, config):
-    for m_name, modul
+    for m_name, module in dict(transformer.named_modules()).items():
+        if re.fullmatch(config.bitfit_modules, m_name):
+            for c_name, layer in dict(module.named_children()
