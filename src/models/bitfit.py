fe4ac16e99a0a2179afc24dfec9fa19b8d006814
lopeve <135252518+lopeve@users.noreply.github.com>
2023-04-23 13:51:53 +0000

    depends

--- src/models/bitfit.py
+++ src/models/bitfit.py
@@ -6,4 +6,7 @@ import re
 def modify_with_bitfit(transformer, config):
     for m_name, module in dict(transformer.named_modules()).items():
         if re.fullmatch(config.bitfit_modules, m_name):
-            for c_name, layer in dict(module.named_children()
+            for c_name, layer in dict(module.named_children()).items():
+                if re.fullmatch(config.bitfit_layers, c_name):
+                    layer.bias = nn.Parameter(torch.zeros(layer.out_features))
+    return transf
