3a98cde0b00cc98914eccb67b371767df1da3493
lopeve <135252518+lopeve@users.noreply.github.com>
2023-05-21 21:19:27 +0000

    merge fixes

--- src/models/fishmask.py
+++ src/models/fishmask.py
@@ -4,4 +4,6 @@ import torch
 
 def fishmask_plugin_on_init(pl_module):
     if pl_module.config.fishmask_mode == "apply":
-   
+        print(f"Load gradient mask from {pl_module.config.fishmask_path}")
+        mask_dict = torch.load(pl_module.config.fishmask_path)
+        
