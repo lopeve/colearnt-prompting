930eba19cddd109e52f4a49ee1f9a67c9a7827ad
lopeve <135252518+lopeve@users.noreply.github.com>
2023-05-24 13:58:19 +0000

    Update readme badges

--- src/models/fishmask.py
+++ src/models/fishmask.py
@@ -6,4 +6,8 @@ def fishmask_plugin_on_init(pl_module):
     if pl_module.config.fishmask_mode == "apply":
         print(f"Load gradient mask from {pl_module.config.fishmask_path}")
         mask_dict = torch.load(pl_module.config.fishmask_path)
-        
+        for param_name, param in pl_module.model.named_parameters():
+            param.stored_mask = mask_dict[param_name].to("cuda")
+
+
+def fishmask_plug
