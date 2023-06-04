6c637a544166342f6806ef433a1a71d1b87b22ba
lopeve <135252518+lopeve@users.noreply.github.com>
2023-06-04 22:24:43 +0000

    update license file

--- src/models/fwh_cuda/fwh_cu.cu
+++ src/models/fwh_cuda/fwh_cu.cu
@@ -1,1 +1,5 @@
-// The codes are from Armen Aghajanyan from f
+// The codes are from Armen Aghajanyan from facebook, from paper
+// Intrinsic Dimensionality Explains the Effectiveness of Language Model Fine-Tuning
+// https://arxiv.org/abs/2012.13255
+
+#include <torch/extension.h>
