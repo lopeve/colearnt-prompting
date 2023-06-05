33b0b284c6ca36be7b3d43a5f0460f68f73f4007
lopeve <135252518+lopeve@users.noreply.github.com>
2023-06-05 06:23:25 +0000

    Minor simplification.

--- src/models/fwh_cuda/fwh_cu.cu
+++ src/models/fwh_cuda/fwh_cu.cu
@@ -3,3 +3,11 @@
 // https://arxiv.org/abs/2012.13255
 
 #include <torch/extension.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <vector>
+
+template <typename scalar_t>
+__global__ void FastWalshHadamardKernel(const int stride, c
