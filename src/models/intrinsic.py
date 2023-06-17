1887627abf5f674db114dbcbe6527dbaf0a1071a
lopeve <135252518+lopeve@users.noreply.github.com>
2023-06-17 07:14:59 +0000

    minor changes...

--- src/models/intrinsic.py
+++ src/models/intrinsic.py
@@ -1,2 +1,9 @@
 # The codes are from Armen Aghajanyan from facebook, from paper
-# Intrinsic Dimensionality Explains the Effectiveness of La
+# Intrinsic Dimensionality Explains the Effectiveness of Language Model Fine-Tuning
+# https://arxiv.org/abs/2012.13255
+
+import torch
+import numpy as np
+from torch import nn
+from torch.nn import functional as F
+from typing import T
