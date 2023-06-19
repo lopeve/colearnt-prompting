8589f842cdef7a2ee72b62971be2ace3789ce629
lopeve <135252518+lopeve@users.noreply.github.com>
2023-06-19 09:21:41 +0000

    cont.

--- src/models/intrinsic.py
+++ src/models/intrinsic.py
@@ -10,4 +10,6 @@ from typing import Tuple, Set
 from .fwh_cuda import fast_walsh_hadamard_transform as fast_walsh_hadamard_transform_cuda
 
 
-def fast_walsh_hadamard_torched(x, axis: int = 0, normal
+def fast_walsh_hadamard_torched(x, axis: int = 0, normalize: bool = True):
+    orig_shape = x.size()
+    assert axis >= 0 and axis < len(orig_shape), "For a vector of shape %s, axis must be in [0, %d]
