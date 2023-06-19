da4948324f10f3506f63eff153fddd86dd823e4e
lopeve <135252518+lopeve@users.noreply.github.com>
2023-06-19 10:14:45 +0000

    Updated Vagrant boxes for Debian

--- src/models/intrinsic.py
+++ src/models/intrinsic.py
@@ -12,4 +12,10 @@ from .fwh_cuda import fast_walsh_hadamard_transform as fast_walsh_hadamard_trans
 
 def fast_walsh_hadamard_torched(x, axis: int = 0, normalize: bool = True):
     orig_shape = x.size()
-    assert axis >= 0 and axis < len(orig_shape), "For a vector of shape %s, axis must be in [0, %d]
+    assert axis >= 0 and axis < len(orig_shape), "For a vector of shape %s, axis must be in [0, %d] but it is %d" % (
+        orig_shape,
+        len(orig_shape) - 1,
+        axis,
+    )
+    h_dim = orig_shape[axis]
+    h_dim_exp =
