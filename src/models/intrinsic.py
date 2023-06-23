e32e9d61e8a94c97cac37e16f7b5458c4f26565a
lopeve <135252518+lopeve@users.noreply.github.com>
2023-06-23 15:16:02 +0000

    explanation

--- src/models/intrinsic.py
+++ src/models/intrinsic.py
@@ -27,4 +27,11 @@ def fast_walsh_hadamard_torched(x, axis: int = 0, normalize: bool = True):
     working_shape_pre = [int(torch.prod(torch.tensor(orig_shape[:axis])))]
     working_shape_post = [int(torch.prod(torch.tensor(orig_shape[axis + 1 :])))]
     working_shape_mid = [2] * h_dim_exp
-    working_shape = working_shape_pre + working_sh
+    working_shape = working_shape_pre + working_shape_mid + working_shape_post
+
+    ret = x.view(working_shape)
+
+    for ii in range(h_dim_exp):
+        dim = ii + 1
+        arrs = torch.chunk(ret, 2, dim=dim)
+        asser
