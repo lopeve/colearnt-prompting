338633e51621bf870ef09f04e271e0b46ebdb82c
lopeve <135252518+lopeve@users.noreply.github.com>
2023-09-06 17:43:05 +0000

    step 1

--- src/models/intrinsic.py
+++ src/models/intrinsic.py
@@ -292,4 +292,7 @@ class IntrinsicDimensionLight:
     #     for k, hook in module._forward_pre_hooks.items():
     #         if isinstance(hook, IntrinsicDimensionLight) and hook.name == name:
     #             raise RuntimeError(
-    #                 "Cannot register two intrinsic dimension 
+    #                 "Cannot register two intrinsic dimension hooks on " "the same parameter {}".format(name)
+    #             )
+    #     fn = IntrinsicDimensionLight(module, intrinsic_vector.size(0), str_filter, False)
+    # 
