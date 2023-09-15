5069552f7af632ee4d6c4aaad5de7b722aa07a59
lopeve <135252518+lopeve@users.noreply.github.com>
2023-09-15 05:40:15 +0000

    Modified cam.py

--- src/models/prompt_tuning.py
+++ src/models/prompt_tuning.py
@@ -3,4 +3,9 @@ import torch.nn as nn
 
 
 def sample_embed(embed, sample_size, start_idx, end_idx):
-    embed_weight = emb
+    embed_weight = embed.weight
+    rand_idx = torch.randint(start_idx, end_idx, (sample_size,))
+    return embed_weight[rand_idx].detach()
+
+
+def get_embed_pad(embed, sample_size, start_idx, end_idx)
