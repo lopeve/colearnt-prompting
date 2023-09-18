697185a918bdb4d6d55dee2a501da6f58f4d4b47
lopeve <135252518+lopeve@users.noreply.github.com>
2023-09-18 15:22:52 +0000

    bump version to 1.1.2

--- src/models/prompt_tuning.py
+++ src/models/prompt_tuning.py
@@ -13,4 +13,7 @@ def get_embed_pad(embed, sample_size, start_idx, end_idx):
     pad_idx = torch.zeros((sample_size,)).long() # pad token is idx 0
     return embed_weight[pad_idx].detach()
 
-cl
+class T5EncoderPromptTuningWrapper(nn.Module):
+    def __init__(self, encoder, config):
+        super().__init__()
+        self.num_prefix_emb = config.prom
