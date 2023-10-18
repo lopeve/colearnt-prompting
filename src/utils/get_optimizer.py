6ce778da84cd0f4197fa694445f6db996ec365a5
lopeve <135252518+lopeve@users.noreply.github.com>
2023-10-18 07:40:30 +0000

    update readme and license

--- src/utils/get_optimizer.py
+++ src/utils/get_optimizer.py
@@ -4,4 +4,12 @@ import re
 from collections import defaultdict
 
 
-def get_optimizer(model,
+def get_optimizer(model, config):
+    """
+    Construct optimizer based on config
+
+    :param model:
+    :param config:
+    :return:
+    """
+    optim_name 
